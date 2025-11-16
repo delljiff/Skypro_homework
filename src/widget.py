dcf9a7de457b185c357d9c9e2e41c29c169c3466
имя фамилия < >
2025-11-16 18:48:19 +0500

    Конечный вид

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,4 @@
-from masks import get_mask_card_number
-from masks import get_mask_account
+from masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(card_or_acc_data: str) -> str:
@@ -13,16 +12,16 @@ def mask_account_card(card_or_acc_data: str) -> str:
         if parts[0].lower() == "счет":
             number_part = parts[-1]
             masked = get_mask_account(number_part)
-            return f'Счет {masked}'
+            return f"Счет {masked}"
     elif number_part.isdigit() and len(number_part) == 16:
         card_num = number_part
         card_title = " ".join(parts[:-1])
         masked_num = get_mask_card_number(card_num)
-        return f'{card_title} {masked_num}'
+        return f"{card_title} {masked_num}"
     else:
-        return 'Неверно введены данные'
+        return "Неверно введены данные"
 
 
 def get_date(unformatted_date: str) -> str:
     """Функция преобразует поступаемую на вход дату в формат ДД.ММ.ГГГГ"""
-    return f'{unformatted_date[8:10]}.{unformatted_date[5:7]}.{unformatted_date[0:4]}'
+    return f"{unformatted_date[8:10]}.{unformatted_date[5:7]}.{unformatted_date[0:4]}"
