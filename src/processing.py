7c494341161c49d35ea43f0e4fd39208049f70e3
имя фамилия < >
2025-11-23 17:55:12 +0500

    добавлен readme

--- src/processing.py
+++ src/processing.py
@@ -1,21 +1,22 @@
 from typing import List
 
-def processing(data: List, state: str='EXECUTED') -> List:
+
+def processing(data: List, state: str = "EXECUTED") -> List:
     """
     Функция сортирует список словарей по заданному параметру state
     """
     if not data:
         return []
     else:
-        return [item for item in data if item.get('state') == state]
+        return [item for item in data if item.get("state") == state]
 
 
-def sort_by_date(next_data: List, reverse: bool=True) -> List:
+def sort_by_date(next_data: List, reverse: bool = True) -> List:
     """
     Функция сортирует список словарей по параметру date по убыванию
     """
     if not next_data:
         return []
     else:
-        sorted_data = sorted(next_data, key=lambda x: x['date'], reverse=reverse)
+        sorted_data = sorted(next_data, key=lambda x: x["date"], reverse=reverse)
         return sorted_data
