0e70f630ad780e80e8a0f11455cd66564018b7c0
имя фамилия < >
2025-12-01 19:52:23 +0500

    Доработка

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
-from typing import List
+from typing import List, Dict
 
 
-def processing(data: List, state: str = "EXECUTED") -> List:
+def filter_by_state(data: List[Dict], state: str = "EXECUTED") -> List[Dict]:
     """
     Функция сортирует список словарей по заранее известному параметру state
     """
@@ -11,12 +11,12 @@ def processing(data: List, state: str = "EXECUTED") -> List:
         return [item for item in data if item.get("state") == state]
 
 
-def sort_by_date(next_data: List, reverse: bool = True) -> List:
+def sort_by_date(transactions: List[Dict], reverse: bool = True) -> List[Dict]:
     """
     Функция для сортировки список словарей по параметру date по убыванию
     """
-    if not next_data:
+    if not transactions:
         return []
     else:
-        sorted_data = sorted(next_data, key=lambda x: x["date"], reverse=reverse)
+        sorted_data = sorted(transactions, key=lambda x: x["date"], reverse=reverse)
         return sorted_data
